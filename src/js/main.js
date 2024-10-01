5d1995ef691c7b0f0241862de09682ae8d05fa3d
Alena <alena.karpov@gmail.com>
2024-10-01 13:55:08 +0100

    feat: js: add actions, classes modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
-import './lib/lib';
+import $ from './lib/lib';
 
 $('div').hide().show();
 $('.active').toggle().toggle();
+$('.active').addClass('hello', 'world');
+$('.active').toggleClass('hello');
+$('.active').on('click', sayHello);
+$('.active').off('click', sayHello);
+$('.active').click(sayHello);
+
+function sayHello() {
+	console.log('hello');
+}
+
+$('button').on('click', function() { 
+	$(this).hide().show().toggleClass('active');
+});
