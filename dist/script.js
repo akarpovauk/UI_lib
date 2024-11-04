0c0ae4e71052f66fc4da4a2e7362cefc771fbddd
Alena <alena.karpov@gmail.com>
2024-11-04 17:14:39 +0000

    ref: js carousel

--- dist/script.js
+++ dist/script.js
@@ -40,8 +40,8 @@ __webpack_require__.r(__webpack_exports__);
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.carousel = function () {
   for (let i = 0; i < this.length; i++) {
     const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width;
-    const slides = this[i].querySelectorAll('.carousel-item');
     const slidesField = this[i].querySelector('.carousel-slides');
+    const slides = this[i].querySelectorAll('.carousel-item');
     const dots = this[i].querySelectorAll('.carousel-indicators li');
 
     //add width to .carousel-slides равную количеству слайдов, чтобы этот блок вмещал их всех
@@ -52,21 +52,25 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.carousel = function () {
       slide.style.width = width;
     });
     let offset = 0;
+    let widthNum = +width.replace(/\D/g, '');
     let slideIndex = 0;
+
     //переключение слайда by clicking right arrow (next)
     (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].querySelector('[data-slide="next"]')).click(e => {
       e.preventDefault();
-      if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
+      if (offset == widthNum * (slides.length - 1)) {
         offset = 0;
       } else {
-        offset += +width.replace(/\D/g, '');
+        offset += widthNum;
       }
       slidesField.style.transform = `translateX(-${offset}px)`;
-      if (slideIndex == slides.length - 1) {
-        slideIndex = 0;
-      } else {
-        slideIndex++;
-      }
+
+      // if (slideIndex == slides.length - 1) {
+      // 	slideIndex = 0;
+      // } else {
+      // 	slideIndex ++;
+      // }
+      slideIndex = offset / widthNum;
       dots.forEach(dot => dot.classList.remove('active'));
       dots[slideIndex].classList.add('active');
     });
@@ -75,27 +79,31 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.carousel = function () {
     (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].querySelector('[data-slide="prev"]')).click(e => {
       e.preventDefault();
       if (offset == 0) {
-        offset = +width.replace(/\D/g, '') * (slides.length - 1);
+        offset = widthNum * (slides.length - 1);
       } else {
-        offset -= +width.replace(/\D/g, '');
+        offset -= widthNum;
       }
       slidesField.style.transform = `translateX(-${offset}px)`;
-      if (slideIndex == 0) {
-        slideIndex = slides.length - 1;
-      } else {
-        slideIndex--;
-      }
+
+      // if (slideIndex == 0) {
+      // 	slideIndex = slides.length - 1;
+      // } else {
+      // 	slideIndex --;
+      // }
+      slideIndex = offset / widthNum;
       dots.forEach(dot => dot.classList.remove('active'));
       dots[slideIndex].classList.add('active');
     });
+    //indicators
     const sliderId = this[i].getAttribute('id');
     (0,_core__WEBPACK_IMPORTED_MODULE_0__["default"])(`#${sliderId} .carousel-indicators li`).click(e => {
       const slideTo = e.target.getAttribute('data-slide-to');
-      slideIndex = slideTo;
-      offset = +width.replace(/\D/g, '') * slideTo;
+
+      // slideIndex = slideTo;
+      offset = widthNum * slideTo;
       slidesField.style.transform = `translateX(-${offset}px)`;
       dots.forEach(dot => dot.classList.remove('active'));
-      dots[slideIndex].classList.add('active');
+      dots[slideTo].classList.add('active');
     });
   }
 };
--- src/js/lib/components/carousel.js
+++ src/js/lib/components/carousel.js
@@ -3,8 +3,8 @@ import $ from '../core';
 $.prototype.carousel = function() {
 	for (let i = 0; i < this.length; i++) {
 		const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width;
-		const slides = this[i].querySelectorAll('.carousel-item');
 		const slidesField = this[i].querySelector('.carousel-slides');
+		const slides = this[i].querySelectorAll('.carousel-item');
 		const dots = this[i].querySelectorAll('.carousel-indicators li');
 
 		//add width to .carousel-slides равную количеству слайдов, чтобы этот блок вмещал их всех
@@ -16,24 +16,26 @@ $.prototype.carousel = function() {
 		});
 
 		let offset = 0;
+		let widthNum = +width.replace(/\D/g, '');
 		let slideIndex = 0;
+		
 		//переключение слайда by clicking right arrow (next)
 		$(this[i].querySelector('[data-slide="next"]')).click((e) => {
 			e.preventDefault();
-			if (offset == (+width.replace(/\D/g, '') * (slides.length - 1))) {
+			if (offset == widthNum * (slides.length - 1)) {
 				offset = 0;
 			} else {
-				offset += +width.replace(/\D/g, '');
+				offset += widthNum;
 			}
 
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
-			if (slideIndex == slides.length - 1) {
-				slideIndex = 0;
-			} else {
-				slideIndex ++;
-			}
-
+			// if (slideIndex == slides.length - 1) {
+			// 	slideIndex = 0;
+			// } else {
+			// 	slideIndex ++;
+			// }
+			slideIndex = offset/widthNum;
 			dots.forEach(dot => dot.classList.remove('active'));
 			dots[slideIndex].classList.add('active');
 		});
@@ -42,32 +44,32 @@ $.prototype.carousel = function() {
 		$(this[i].querySelector('[data-slide="prev"]')).click((e) => {
 			e.preventDefault();
 			if (offset == 0) {
-				offset = +width.replace(/\D/g, '') * (slides.length - 1);
+				offset = widthNum * (slides.length - 1);
 			} else {
-				offset -= +width.replace(/\D/g, '');
+				offset -= widthNum;
 			}
 
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
-			if (slideIndex == 0) {
-				slideIndex = slides.length - 1;
-			} else {
-				slideIndex --;
-			}
-
+			// if (slideIndex == 0) {
+			// 	slideIndex = slides.length - 1;
+			// } else {
+			// 	slideIndex --;
+			// }
+			slideIndex = offset/widthNum;
 			dots.forEach(dot => dot.classList.remove('active'));
 			dots[slideIndex].classList.add('active');
 		});
-
+		//indicators
 		const sliderId = this[i].getAttribute('id');
 		$(`#${sliderId} .carousel-indicators li`).click(e => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 
-			slideIndex = slideTo;
-			offset = +width.replace(/\D/g, '') * slideTo;
+			// slideIndex = slideTo;
+			offset = widthNum * slideTo;
 			slidesField.style.transform = `translateX(-${offset}px)`;
 			dots.forEach(dot => dot.classList.remove('active'));
-			dots[slideIndex].classList.add('active');
+			dots[slideTo].classList.add('active');
 		});
 	}
 };
